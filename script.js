96910183e59ddd9e88d953a9ca327785babe5493
Ogunmola <erioluwadeborah@gmail.com>
2024-01-22 12:41:04 +0100

    redesign footer

--- script.js
+++ script.js
@@ -23,3 +23,70 @@ function logInputInfo() {
     console.log('Name:', inputName);
     console.log('Email:', inputEmail);
 }
+
+
+
+
+
+const rightBtn = document.querySelector('.right')
+const allPics = document.querySelectorAll('.pic')
+const leftBtn = document.querySelector('.left')
+const auto  = false
+const  timer = 3000
+let autoSlide
+
+rightBtn.addEventListener('click', () => {
+    nextSlide() 
+    clearInterval (autoSlide)
+    if(auto){
+        autoSlide = setInterval(nextSlide, timer)
+    }
+
+})
+leftBtn.addEventListener('click',  () => {
+    previousSlide()
+    clearInterval (autoSlide)
+    if(auto){
+        autoSlide = setInterval(previousSlide, timer)
+    }
+
+})
+
+function nextSlide(){
+    const current = document.querySelector('.current')
+
+    current.classList.remove('current')
+  
+
+    if(current.nextElementSibling){
+        current.nextElementSibling.classList.add('current')
+
+    }
+    else {
+        allPics[0].classList.add('current')
+    }
+  
+} 
+
+
+ 
+function previousSlide(){
+    const current = document.querySelector('.current')
+
+    current.classList.remove('current')   
+    if(current.previousElementSibling){
+        current.previousElementSibling.classList.add('current')
+    }
+    else {
+        allPics[3].classList.add('current')
+    }
+
+}
+
+
+if(auto){
+    autoSlide = setInterval(nextSlide, timer)
+}
+if(auto){
+    autoSlide = setInterval(previousSlide, timer)
+}
