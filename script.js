7c1d9b7340f13443e2885b91f2ce072e93327aff
Ogunmola <erioluwadeborah@gmail.com>
2024-01-22 21:18:17 +0100

    applying favicon

--- script.js
+++ script.js
@@ -1,92 +1,50 @@
-let circles = document.querySelectorAll('#circle');
+// let circles = document.querySelectorAll("#circle");
 
-function changeColors() {
-    for (let i = 0; i < circles.length; i++) {
-        circles[i].style.backgroundColor = getRandomColor();
-    }
-}
+let logo = document.querySelector("#nav-bar-logo");
+let logo_two = document.querySelector(".nav-bar-two");
 
-function getRandomColor() {
-    return '#' + Math.floor(Math.random() * 16777215).toString(16);
-}
+(function () {
+  function updateReadProgressBar() {
+    const root = document.documentElement;
+    const winScrollTop = root.scrollTop;
+    const winScrollHeight = root.scrollHeight;
+    const height = root.scrollHeight - root.clientHeight;
+    const progressBarWidth = (winScrollTop / height) * 100;
 
-setInterval(changeColors, 1000);
-
-
-
-
-function logInputInfo() {
-  
-    let inputName = document.getElementById('inputName').value;
-    let inputEmail = document.getElementById('inputEmail').value;
-
-    console.log('Name:', inputName);
-    console.log('Email:', inputEmail);
-}
+    root.style.setProperty("--scroll-progress-width", progressBarWidth + "%");
 
 
-
-
-
-const rightBtn = document.querySelector('.right')
-const allPics = document.querySelectorAll('.pic')
-const leftBtn = document.querySelector('.left')
-const auto  = false
-const  timer = 3000
-let autoSlide
-
-rightBtn.addEventListener('click', () => {
-    nextSlide() 
-    clearInterval (autoSlide)
-    if(auto){
-        autoSlide = setInterval(nextSlide, timer)
+    if (progressBarWidth <= 9.656953225337398) {
+      logo_two.classList.add("disapper");
+    } else {
+      logo_two.classList.remove("disapper");
     }
 
-})
-leftBtn.addEventListener('click',  () => {
-    previousSlide()
-    clearInterval (autoSlide)
-    if(auto){
-        autoSlide = setInterval(previousSlide, timer)
+    if (progressBarWidth >= 92.71756141626193) {
+      logo_two.classList.add("disapper");
     }
+  }
 
-})
-
-function nextSlide(){
-    const current = document.querySelector('.current')
-
-    current.classList.remove('current')
-  
-
-    if(current.nextElementSibling){
-        current.nextElementSibling.classList.add('current')
-
-    }
-    else {
-        allPics[0].classList.add('current')
-    }
-  
-} 
+  window.addEventListener("load", updateReadProgressBar);
+  window.addEventListener("scroll", updateReadProgressBar);
+})();
 
 
+// Function to save user information and email to local storage
+function saveUserInfo() {
  
-function previousSlide(){
-    const current = document.querySelector('.current')
-
-    current.classList.remove('current')   
-    if(current.previousElementSibling){
-        current.previousElementSibling.classList.add('current')
-    }
-    else {
-        allPics[3].classList.add('current')
-    }
-
-}
+  let userNameInput = document.getElementById("name");
+  let userEmailInput = document.getElementById("email_id");
 
 
-if(auto){
-    autoSlide = setInterval(nextSlide, timer)
-}
-if(auto){
-    autoSlide = setInterval(previousSlide, timer)
+  let userNameValue = userNameInput.value;
+  let userEmailValue = userEmailInput.value;
+
+
+  if (userNameValue.trim() !== "" && userEmailValue.trim() !== "") {
+     
+      localStorage.setItem("userInformation", userNameValue);
+      localStorage.setItem("userEmail", userEmailValue);
+
+  }
 }
